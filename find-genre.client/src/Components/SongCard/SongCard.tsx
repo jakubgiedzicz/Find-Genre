183a15062c18a7af14aa11822a74baae10653bfb
Kubutek <jakubgiedzicz@gmail.com>
2024-10-31 15:57:37 +0100

    Song page, songcard

---
+++ find-genre.client/src/Components/SongCard/SongCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function SongCard() {
+    return (
+        <>
+            <h2>Witch house</h2>
+            <p>Witch house is a microgenre of electronic music that is musically characterized by high-pitched keyboard effects, heavily layered basslines and trap-style drum loops, while it aesthetically employs occult and gothic-inspired themes.</p>
+            <iframe width="500" height="400" src="https://www.youtube.com/embed/S40KVjsdCCM" title="± River Bones - Tyrant ±" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+            </iframe>
+            <div>Tags:</div>
+            <div>dark</div><div>electronic</div>
+        </>
+    );
+}
+
+export default SongCard;
