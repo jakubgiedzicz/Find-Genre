3849d8a5e03710a1c207a200bdf0cc16ffbf36c9
Kubutek <jakubgiedzicz@gmail.com>
2024-11-09 22:25:24 +0100

    interface refactor/added id's, clearing input field

--- find-genre.client/src/Pages/Home/Home.tsx
+++ find-genre.client/src/Pages/Home/Home.tsx
@@ -1,28 +1,33 @@
-import { Autocomplete, Box, Divider, Flex, Grid, InputBase, Pill, Stack, Text, Title } from "@mantine/core";
+import { Autocomplete, Box, Button, Divider, Flex, Grid, InputBase, Pill, Stack, Text, Title } from "@mantine/core";
 import { useEffect, useState } from "react";
 import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
 import classes from './Home.module.css'
-interface Itag {
+interface ITagData {
     value: string,
-    disabled: boolean
+    disabled: boolean,
+}
+interface ITagState extends ITagData {
+    id: number
 }
 
 function Home() {
     const icon = <MagnifyingGlassIcon />
+    const [count, setCount] = useState(1)
     const [value, setValue] = useState('');
-    const [required, setRequired] = useState<Itag[]>([]);
-    const [optional, setOptional] = useState<Itag[]>([]);
-    const [exclude, setExclude] = useState<Itag[]>([]);
+    const [required, setRequired] = useState<ITagState[]>([]);
+    const [optional, setOptional] = useState<ITagState[]>([]);
+    const [exclude, setExclude] = useState<ITagState[]>([]);
     const tagsL = ['dark', 'electronic', 'choir', 'popular', 'niche', 'club', 'solitude', 'tiktok', 'fast', 'slow', 'cheerful', 'old', 'classic', 'western', 'korean', 'european']
-    const [dataR, setDataR] = useState <Itag[]> ([
-            { value: 'React', disabled: false },
-            { value: 'Angular', disabled: false },
-            { value: 'Vue', disabled: false },
-            { value: 'Svelte', disabled: false },
-        ])
-    
+    const [dataR, setDataR] = useState<ITagData[]>([
+        { value: 'React', disabled: false },
+        { value: 'Angular', disabled: false },
+        { value: 'Vue', disabled: false },
+        { value: 'Svelte', disabled: false },
+    ])
+
+    /*Add new object to required array, update bool value in dataR*/
     const handleOptionSubmit = (val: string) => {
-        setRequired([...required, { value: val, disabled: true }])
+        setRequired([...required, { value: val, disabled: true, id: count }])
         setDataR(dataR.map(x => {
             if (x.value === val) {
                 return { ...x, disabled: true }
@@ -30,7 +35,14 @@ function Home() {
                 return x
             }
         }))
+        setCount(count + 1)
     }
+    /*Clear input field on submit after 350ms*/
+    useEffect(() => {
+        setTimeout(() =>
+            setValue('')
+        , 350)
+    }, [required])
     return (
         <>
             <Stack pt='4em'>
@@ -48,7 +60,8 @@ function Home() {
                             <Divider my='md' />
                             <InputBase component="div" multiline>
                                 <Pill.Group>
-                                    {required.map((element) => <Pill size='xl' withRemoveButton>{element.value}</Pill>)}
+                                    {/*Filter out selected element out of required, set update bool on the same element in dataR*/}
+                                    {required.map((element) => <Pill size='xl' onRemove={() => { setRequired(required.filter(x => { return x.id != element.id })); setDataR(dataR.map(x => { if (x.value === element.value) { return { ...x, disabled: false } } else { return x } })) }} key={element.id} withRemoveButton>{element.value}</Pill>)}
                                 </Pill.Group>
                             </InputBase>
                         </Grid.Col>
@@ -58,21 +71,21 @@ function Home() {
                             <Divider my='md' />
                             <InputBase component="div" multiline>
                                 <Pill.Group>
-                                    {optional.map((element) => <Pill size='xl' withRemoveButton>{element.value}</Pill>)}
+                                    {optional.map((element) => <Pill size='xl' key={element.id} withRemoveButton>{element.value}</Pill>)}
                                 </Pill.Group>
                             </InputBase>
                         </Grid.Col>
                         <Divider orientation='vertical' />
-                    <Grid.Col span={4}>
+                        <Grid.Col span={4}>
                             <Title order={2} ta='center'>Exclude</Title>
                             <Divider my='md' />
                             <InputBase component="div" multiline>
                                 <Pill.Group>
-                                    {exclude.map((element) => <Pill size='xl' withRemoveButton>{element.value}</Pill>)}
+                                    {exclude.map((element) => <Pill size='xl' key={element.id} withRemoveButton>{element.value}</Pill>)}
                                 </Pill.Group>
                             </InputBase>
-                    </Grid.Col>
-                </Grid>
+                        </Grid.Col>
+                    </Grid>
                 </Flex>
             </Stack>
         </>
