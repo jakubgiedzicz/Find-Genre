a06c748dc18240a532836bf6ed91c9e2a558a3d0
JakubGiedzicz <jakubgiedzicz@gmail.com>
2025-03-05 13:37:31 +0100

    home styling

--- find-genre.client/src/Components/HomeTagBox/HomeTagBox.tsx
+++ find-genre.client/src/Components/HomeTagBox/HomeTagBox.tsx
@@ -3,67 +3,75 @@ import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
 import styles from "./HomeTagBox.module.css";
 import { ITagData } from "../../Types/hometag";
 interface Props {
-  tag: ITagData;
-  update: (tag: ITagData, state: string) => void;
+    tag: ITagData;
+    update: (tag: ITagData, state: string) => void;
 }
 
 const HomeTagBox = (props: Props) => {
-  const { colorScheme, setColorScheme } = useMantineColorScheme();
-  const tagBackground = () => {
-    if (colorScheme === "dark") {
-      if (props.tag.state === "include") {
-        return styles.include_dark;
-      } else if (props.tag.state === "exclude")
-        return styles.exclude_dark;
-    } else {
-      if (props.tag.state === "include") {
-        return styles.include_light;
-      } else if (props.tag.state === "exclude")
-        return styles.exclude_light;
+    const { colorScheme, setColorScheme } = useMantineColorScheme();
+    const tagBackground = () => {
+        if (colorScheme === "dark") {
+            if (props.tag.state === "include") {
+                return styles.include_dark;
+            } else if (props.tag.state === "exclude")
+                return styles.exclude_dark;
+        } else {
+            if (props.tag.state === "include") {
+                return styles.include_light;
+            } else if (props.tag.state === "exclude")
+                return styles.exclude_light;
+        }
+    };
+    const buttonVariant = () => {
+        if (colorScheme === "dark") {
+            return "light";
+        } else return "filled";
+    };
+    const handleClick = (status: string) => {
+        if (status === "include") {
+            if (props.tag.state === "include") {
+                props.update(props.tag, "default");
+            } else {
+                props.update(props.tag, "include");
+            }
+        } else {
+            if (props.tag.state === "exclude") {
+                props.update(props.tag, "default");
+            } else {
+                props.update(props.tag, "exclude");
+            }
+        }
+    };
+    const getClassname = () => {
+        return tagBackground() + " " + styles.container_padding + ' ' + styles.container_shadow
     }
-  };
-  const buttonVariant = () => {
-    if (colorScheme === "dark") {
-      return "light";
-    } else return "filled";
-  };
-  const handleClick = (status: string) => {
-    if (status === "include") {
-      if (props.tag.state === "include") {
-        props.update(props.tag, "default");
-      } else {
-        props.update(props.tag, "include");
-      }
-    } else {
-      if (props.tag.state === "exclude") {
-        props.update(props.tag, "default");
-      } else {
-        props.update(props.tag, "exclude");
-      }
-    }
-  };
 
-  return (
-    <>
-      <Group justify="space-between" className={tagBackground() + " " + styles.container_padding + ' ' + styles.container_shadow}>
-        <Button
-          variant={buttonVariant()}
-          color="green"
-          onClick={() => handleClick("include")}
-        >
-          <PlusIcon />
-        </Button>
-        <Text>{props.tag.value}</Text>
-        <Button
-          variant={buttonVariant()}
-          color="red"
-          onClick={() => handleClick("exclude")}
-        >
-          <MinusIcon />
-        </Button>
-      </Group>
-    </>
-  );
+    return (
+        <>
+            <Group justify="space-between" className={getClassname()}>
+                <Button
+                    variant={buttonVariant()}
+                    color="green"
+                    onClick={() => handleClick("include")}
+                    className={styles.button_green}
+                    miw={"0.5em"}
+                    bd={"none"}
+                >
+                    <PlusIcon width={"1em"} />
+                </Button>
+                <Text>{props.tag.value}</Text>
+                <Button
+                    variant={buttonVariant()}
+                    color="red"
+                    onClick={() => handleClick("exclude")}
+                    miw={"0.5em"}
+                    bd={"none"}
+                >
+                    <MinusIcon width={"1em"} />
+                </Button>
+            </Group>
+        </>
+    );
 };
 
 export default HomeTagBox;
