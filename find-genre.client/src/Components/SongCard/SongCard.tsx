59608ef2e02aad4cff9bc6d1a9e9df6265c80a0f
JakubGiedzicz <jakubgiedzicz@gmail.com>
2025-03-05 18:28:19 +0100

    hsl color fix, further content styling

--- find-genre.client/src/Components/SongCard/SongCard.tsx
+++ find-genre.client/src/Components/SongCard/SongCard.tsx
@@ -1,9 +1,11 @@
-import { Title, Text, Pill, InputBase, Stack, Center, Group, Badge, Box, Image, Container, useMantineTheme, useComputedColorScheme } from "@mantine/core";
+﻿import { Title, Text, Pill, InputBase, Stack, Center, Group, Badge, Box, Image, Container, useMantineTheme, useComputedColorScheme, Card, Flex } from "@mantine/core";
 import { genreType } from "../../Types/api";
 import { useEffect, useState } from "react";
 import { Carousel } from "@mantine/carousel";
 import '@mantine/carousel/styles.css';
 import styles from './SongCard.module.css'
+import { Link } from "react-router-dom";
+import bc_light from '../../assets/bandcamp-logotype-light-128.png'
 function SongCard({ props }: { props: genreType }) {
     const theme = useMantineTheme();
     const scheme = useComputedColorScheme();
@@ -12,7 +14,7 @@ function SongCard({ props }: { props: genreType }) {
     }
     return (
         <Stack mx={'20%'}>
-            <Group justify="center" align="flex-start" wrap="nowrap" mt="64" gap="lg">
+            <Group justify="center" align="center" wrap="nowrap" mt="64" gap="lg">
                 <Stack>
                     <Title order={1}>{props.name}</Title>
                     <Text>
@@ -49,12 +51,31 @@ function SongCard({ props }: { props: genreType }) {
                     </Carousel>
                 </Box>
             </Group>
-            <Title>
+            <Title order={1}>
                 Aesthetic
             </Title>
             <Text>
                 The witch house visual aesthetic includes occultism, witchcraft, shamanism, terror and horror-inspired artworks, collages and photographs. Common typographic elements in titles, such as by Salem and White Ring, include triangles, crosses and Unicode symbols, which are seen by some as a method of gatekeeping (in an effort to keep the scene underground and more difficult to search for on the Internet).
             </Text>
+            <Title order={1 }>
+            Artists
+            </Title>
+            <Card>
+                <Title order={2}>ΔXIUS LIИK</Title>
+                <Card.Section className={styles.spotify_border} p={"1em"}>
+                    <iframe src="https://open.spotify.com/embed/artist/1C6yCV9Y7kycveSPJr0un9?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                </Card.Section>
+            </Card>
+            <Card>
+                <Title order={2}>Fraunhofer Diffraction</Title>
+                <Card.Section className={styles.spotify_border} p={"1em"}>
+                    <Flex justify={"center"} align={"center"}>
+                        <Link to="https://ivoryrite.bandcamp.com/" target={"_blank"}>
+                            <Image src={bc_light} width={442} height={128} />
+                        </Link>
+                    </Flex>
+                </Card.Section>
+            </Card>
         </Stack>
     );
 }
