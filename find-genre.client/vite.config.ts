d37700f311f3447a191ecaa551643c1a51bb4181
JakubGiedzicz <jakubgiedzicz@gmail.com>
2025-03-10 11:22:30 +0100

    vite config base fix

--- find-genre.client/vite.config.ts
+++ find-genre.client/vite.config.ts
@@ -38,6 +38,7 @@ import plugin from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/Find-Genre/",
     plugins: [plugin()],
     resolve: {
         alias: {
