09d69f1333c00d03a044901e288be4653de2e76b
JakubGiedzicz <jakubgiedzicz@gmail.com>
2025-03-15 13:07:30 +0100

    switch from modules to css

--- find-genre.client/src/Components/HomeTagBox/HomeTagBox.tsx
+++ find-genre.client/src/Components/HomeTagBox/HomeTagBox.tsx
@@ -2,6 +2,7 @@ import { Button, Group, Text, useMantineColorScheme } from "@mantine/core";
 import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
 import styles from "./HomeTagBox.module.css";
 import { ITagData, tagStateType } from "../../Types/hometag";
+import './HomeTagBox.css'
 interface Props {
     tag: ITagData;
     update: (tag: ITagData, state: tagStateType) => void;
@@ -20,7 +21,8 @@ const HomeTagBox = (props: Props) => {
                 return styles.include_light;
             } else if (props.tag.state === "exclude")
                 return styles.exclude_light;
-        }
+            }
+        return 
     };
     const buttonVariant = () => {
         if (colorScheme === "dark") {
@@ -28,39 +30,32 @@ const HomeTagBox = (props: Props) => {
         } else return "filled";
     };
     const handleClick = (status: tagStateType) => {
-        if (status === "include") {
-            if (props.tag.state === "include") {
-                props.update(props.tag, "default");
-            } else {
-                props.update(props.tag, "include");
-            }
+        if (status === props.tag.state) {
+            props.update(props.tag, "default")
         } else {
-            if (props.tag.state === "exclude") {
-                props.update(props.tag, "default");
-            } else {
-                props.update(props.tag, "exclude");
-            }
+            props.update(props.tag, status)
         }
     };
     const getHover = () => {
         return colorScheme == "light" ? styles.light_box : styles.dark_box
     }
-    const getClassname = () => {
-        return tagBackground() + " " + styles.container_padding + ' ' + getHover() + ' ' + getHighlight()
-    }
+    //const getClassname = () => {
+    //    return tagBackground() + " " + styles.container_padding + ' ' + getHover() + ' ' + getHighlight()
+    //}
+    
     const getHighlight = () => {
         if (props.tag.state != "default") {
             if (colorScheme == "light") {
-                return styles.light_background
+                return 'light_background'
             } else if (colorScheme == "dark") {
-                return styles.dark_background
+                return 'dark_background'
             }
         }
     }
 
     return (
         <>
-            <Group justify="space-between" className={getClassname()}>
+            <Group justify="space-between" className={`${props.tag.state}_${colorScheme} container_padding ${colorScheme}_box ${getHighlight()}`}>
                 <Button
                     variant={buttonVariant()}
                     color="green"
