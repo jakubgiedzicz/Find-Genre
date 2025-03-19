efea7611a64d20f2941b30a7dedabb91cb00ddc8
JakubGiedzicz <jakubgiedzicz@gmail.com>
2025-03-19 14:44:00 +0100

    styling, media queries

--- find-genre.client/src/Components/DetailsIntro/DetailsIntro.tsx
+++ find-genre.client/src/Components/DetailsIntro/DetailsIntro.tsx
@@ -1,4 +1,4 @@
-import { Stack, Title, Box, Group, Badge, Text, useComputedColorScheme, useMantineTheme, SimpleGrid } from "@mantine/core";
+import { Stack, Title, Box, Group, Badge, Text, useComputedColorScheme, useMantineTheme, SimpleGrid, Center } from "@mantine/core";
 import { ISubgenre, ITag } from "../../Types/api";
 import styles from './DetailsIntro.module.css'
 import { Carousel } from "@mantine/carousel";
@@ -11,41 +11,43 @@ function DetailsIntro({ name, desc, tags, subgenres, examples }: { name: string,
     }
   return (
       <Stack>
-          <SimpleGrid cols={{ base: 2 }}>
-              <Stack justify="center">
+          <SimpleGrid cols={{ base: 1, xl: 2 }} verticalSpacing="md" spacing="md">
+              <Stack justify="center" align="center">
                   <Title order={1} id={name}>{name}</Title>
-                      <Text>
+                  <Text w="80%">
                           {desc}
                       </Text>
               </Stack>
-              <Box w={500} h={400}>
-                  <Carousel withIndicators controlSize={40} slideSize="100%" loop>
-                      {examples && examples.map((i) => (<Carousel.Slide key={i}><iframe
-                          width="500"
-                          height="400"
-                          loading="lazy"
-                          src={`https://www.youtube.com/embed/${i}`}
-                          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                          referrerPolicy="strict-origin-when-cross-origin"
-                      ></iframe></Carousel.Slide>))}
-                  </Carousel>
-              </Box>
-              <Box className={styles.container_radius}>
+              <Center>
+                  <Box w={500} h={400}>
+                      <Carousel withIndicators controlSize={40} slideSize="100%" loop>
+                          {examples && examples.map((i) => (<Carousel.Slide key={i}><iframe
+                              width="500"
+                              height="400"
+                              loading="lazy"
+                              src={`https://www.youtube.com/embed/${i}`}
+                              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                              referrerPolicy="strict-origin-when-cross-origin"
+                          ></iframe></Carousel.Slide>))}
+                      </Carousel>
+                  </Box>
+              </Center>
+              <Stack className={styles.container_radius} align="center">
                   <Title order={2} id="Tags">Tags</Title>
                   <Group>
                       {tags.map((element) => (
                           <Badge color="indigo" variant="light" key={element.tagId}>{element.name}</Badge>
                       ))}
                   </Group>
-              </Box>
-              {subgenres && <Box className={styles.container_radius}>
+              </Stack>
+              {subgenres && <Stack className={styles.container_radius} align="center">
                   <Title order={2} id="Subgenres">Subgenres</Title>
                   <Group>
                       {subgenres && subgenres.map((element) => (
                           <Badge key={element.subgenreId} color="indigo" variant="light">{element.name}</Badge>
                       ))}
                   </Group>
-              </Box>}
+              </Stack>}
           </SimpleGrid>
       </Stack>
   );
