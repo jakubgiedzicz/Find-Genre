0d0883440ad83aeff4ed6f52ad7b81166ab8e02f
JakubGiedzicz <jakubgiedzicz@gmail.com>
2025-03-24 21:13:37 +0100

    show text on hover

--- find-genre.client/src/Components/GenreCardSmall/GenreCardSmall.tsx
+++ find-genre.client/src/Components/GenreCardSmall/GenreCardSmall.tsx
@@ -1,11 +1,13 @@
 import { Card, Text } from "@mantine/core";
 import styles from './GenreCardSmall.module.css'
+import { useHover } from "@mantine/hooks";
 
 function GenreCardSmall({ title, description }: { title: string, description: string }) {
+    const { hovered, ref } = useHover()
     return (
-        <Card shadow="md" className={styles.similar_card}>
+        <Card shadow="md" className={styles.similar_card} ref={ref}>
             <Text size="xl" fw={500}>{title}</Text>
-            <Text size="lg" lineClamp={4}>
+            <Text size="lg" lineClamp={hovered?10:3}>
                 {description}
             </Text>
         </Card>
